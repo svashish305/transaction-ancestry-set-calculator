ed38f4ea0902a99d444a08a72b0febf566722f3d
Shubhashish Verma <svashish305@gmail.com>
2023-04-10 15:44:42 +0530

    tested script

---
+++ README.md
@@ -0,0 +1,27 @@
+# A basic transaction ancestry set calculator
+
+## Run Instructions
+
+```nodejs
+npm install
+npm run start
+```
+
+### Code Explanation
+This script in index.js retrieves transaction data from a Bitcoin block, constructs a tree of ancestry relationships between the transactions, and then displays the largest ancestry set for the block.
+
+The script starts by importing required packages including dotenv for loading environment variables, axios for making HTTP requests, and print-tree for displaying the ancestry tree.
+
+The script then loads environment variables from a .env file using the dotenv package. It expects BLOCK_HEIGHT and BLOCKSTREAM_API_URL to be defined in the .env file.
+
+The script defines a transactionMap object to store transaction data, and a rootNode object to serve as the root node for the ancestry tree. The rootNode object has an id property set to the block height, and parentMap and parents properties to store ancestry relationships.
+
+The script defines several functions to interact with the Blockstream API, including getBlockHashFromBlockHeight to get the block hash from the block height, and getTransactionsFromBlockHash to get a list of transactions for a given block hash.
+
+The script defines the constructNodes function to create node objects for each transaction in the list, and adds each node to the transactionMap object. The addAncestor function is defined to add the ancestry relationship between a transaction and its ancestors. The getAncestorList function is defined to recursively retrieve the list of all ancestors of a given node.
+
+The script defines the displayAncestryTree function to display the ancestry tree for a given transaction, and the getLargestAncestorTransactionId function to determine the transaction ID for the largest ancestry set for the block.
+
+Finally, the script defines the displayLargestAncestrySet function to display the largest ancestry set for a given transaction, and the displayLargestAncestrySets function to display the largest ancestry set for the block and the entire ancestry tree.
+
+The script wraps the code in an async function and immediately invokes it using an IIFE (Immediately Invoked Function Expression) syntax to start the script execution.
---
+++ index.js
@@ -0,0 +1,153 @@
+import * as dotenv from "dotenv";
+import axios from "axios";
+import printTree from "print-tree";
+
+dotenv.config();
+
+const { BLOCK_HEIGHT, BLOCKSTREAM_API_URL } = process.env;
+
+const transactionMap = {};
+const rootNode = {
+  id: BLOCK_HEIGHT,
+  parentMap: {},
+  parents: [],
+};
+
+const getBlockHashFromBlockHeight = async (blockHeight) => {
+  try {
+    const config = {
+      method: "get",
+      url: `${BLOCKSTREAM_API_URL}/block-height/${BLOCK_HEIGHT}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data: blockHash } = await axios(config);
+    return blockHash;
+  } catch (error) {
+    console.log("Error fetching block hash from block height", error);
+    return null;
+  }
+};
+
+const getTransactionsFromBlockHash = async (blockHash) => {
+  const upToTransactionCount = 25;
+  let startIndex = 0;
+  let allTransactions = [];
+
+  while (true) {
+    try {
+      const config = {
+        method: "get",
+        url: `${BLOCKSTREAM_API_URL}/block/${blockHash}/txs/${
+          startIndex * upToTransactionCount
+        }`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const { data: transactions } = await axios(config);
+      allTransactions = allTransactions.concat(transactions);
+      startIndex++;
+    } catch (error) {
+      break;
+    }
+  }
+
+  return allTransactions;
+};
+
+const constructNodes = (transactions, node) => {
+  const nodes = [];
+  transactions.forEach((transaction) => {
+    if (!transactionMap[transaction.txid]) {
+      const currentNode = {
+        id: transaction.txid,
+        parentMap: {},
+        parents: [],
+        vin: transaction.vin,
+      };
+
+      nodes.push(currentNode);
+      transactionMap[transaction.txid] = currentNode;
+    }
+  });
+
+  node.parentMap = transactionMap;
+  node.parents = node.parents.concat(nodes);
+
+  return nodes;
+};
+
+const addAncestor = (transaction, node) => {
+  const currentNode = transactionMap[transaction.txid];
+  if (currentNode && !node.parentMap[currentNode.id]) {
+    node.parentMap[currentNode.id] = currentNode;
+    node.parents.push(currentNode);
+    currentNode.vin.forEach((inputTxn) => addAncestor(inputTxn, node));
+  }
+};
+
+const getAncestorList = (node) => {
+  let list = [node];
+  Object.keys(node.parentMap).forEach((transaction) => {
+    list = list.concat(getAncestorList(node.parentMap[transaction]));
+  });
+  return list;
+};
+
+const displayAncestryTree = (transactionId, limit = null) => {
+  console.log(`Ancestry set for transaction ${transactionId}: \n`);
+  const listLimit = limit || transactionMap[transactionId].list.length;
+  const list = transactionMap[transactionId].list.slice(0, listLimit);
+  console.log(" -" + list.map((node) => node.id).join("\r\n -"));
+  console.log("\n");
+};
+
+const getLargestAncestorTransactionId = (transactionMap) => {
+  let largestAncestrySetLength = -1;
+  let largestAncestrySetTxId = null;
+  console.log(
+    `Ancestry set for all transactions in the block ${BLOCK_HEIGHT}: \n`
+  );
+  Object.keys(transactionMap).forEach((transactionId) => {
+    const list = getAncestorList(transactionMap[transactionId]);
+    transactionMap[transactionId].list = list.slice(1);
+    if (list.length > largestAncestrySetLength) {
+      largestAncestrySetLength = list.length;
+      largestAncestrySetTxId = transactionId;
+    }
+    displayAncestryTree(transactionId);
+  });
+  return largestAncestrySetTxId;
+};
+
+const displayLargestAncestrySet = (transactionId, limit = 10) => {
+  console.log("Largest Ancestry Set - 10 transactions: \n");
+  displayAncestryTree(transactionId, limit);
+};
+
+const displayLargestAncestrySets = () => {
+  const largestAncestorTxId = getLargestAncestorTransactionId(transactionMap);
+  displayLargestAncestrySet(largestAncestorTxId);
+  printTree(
+    rootNode,
+    (rootNode) => rootNode.id,
+    (rootNode) => rootNode.parents
+  );
+};
+
+(async () => {
+  try {
+    const blockHash = await getBlockHashFromBlockHeight(BLOCK_HEIGHT);
+    let allBlockTransactions = await getTransactionsFromBlockHash(blockHash);
+    allBlockTransactions = allBlockTransactions.slice(1);
+    const nodes = constructNodes(allBlockTransactions, rootNode);
+    nodes.forEach((node) => {
+      node.vin.forEach((inputTxn) => addAncestor(inputTxn, node));
+    });
+    displayLargestAncestrySets();
+  } catch (error) {
+    console.log(error);
+  }
+})();
